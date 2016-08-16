b2f96d95c3a6e819f18bf8337244997bfedd1411
Brent Minchew <minchewb@gmail.com>
2016-08-16 09:30:56 -0700

    updated gdal write routine to exclude null value for ENVI formatted images

--- pysar/image/write.py
+++ pysar/image/write.py
@@ -175,7 +175,7 @@ def writeRaster(z,filename,filetype='GTiff',x=None,y=None,null=np.nan,coords=Non
 
       band = rast.GetRasterBand(1)
       band.WriteArray(z)   ### write z data 
-      if null is not None:
+      if null is not None and filetype is not 'ENVI':
          band.SetNoDataValue(null)
       band.FlushCache()   
    except:
